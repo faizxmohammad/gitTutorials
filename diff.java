378805262c2d8c4aa2241920cd092bea7c37ee9f
faiz mohammad <faizmohd11251@gmail.com>
2022-01-29 00:34:01 +0530

    added implementation for diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,7 @@
 public class diff {
-    
+    public static void main(String[] args) {
+        int a = 23, b = 43;
+        int  c = a - b;
+        System.out.println("now the sum is = " +c);
+    }
 }
