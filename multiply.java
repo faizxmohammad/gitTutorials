af2be8ed0f01168532ff07791363806f18feda54
faiz mohammad <faizmohd11251@gmail.com>
2022-01-29 00:20:06 +0530

    added multiply.java

--- multiply.java
+++ multiply.java
@@ -1,7 +1,7 @@
-public class sum{
+public class multiply {
     public static void main(String[] args) {
         int a = 23, b = 43;
-        int  c = a + b;
+        int  c = a * b;
         System.out.println("now the sum is = "+c);
     }
 }
